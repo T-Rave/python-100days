11f55de4c3bf4a3305e6925284eeecadfd4278f5
T-Rave <t-rave@pop-os.localdomain>
2021-08-06 12:43:29 -0500

    Blackjack project clean up

--- blackjack-capstone.py
+++ blackjack-capstone.py
@@ -1,6 +1,6 @@
 import random
-#def start():
-#    player
+
+# Gather human players and setup players dict. Deal two cards to start game
 def game_setup():
     player_count = int(input("How many players?\n"))
     for player in range(player_count):
@@ -12,19 +12,17 @@ def game_setup():
     while first_hand < 2:
         for player in players:
             deal(player)
-            #print(players)
         first_hand += 1
 
+# Deal cards to players and dealer
 def deal(player_name):
     dealt_card = random.choice(dealer_cards)
     players[player_name]['cards_in_hand'].append(dealt_card)
     dealer_cards.remove(dealt_card)
     print(players[player_name]['cards_in_hand'])
     card_total(player_name,royalty(player_name,dealt_card))
-    #print(dealer_cards)
-    #if player_name != 'computer':
-    #    print(f"You have been dealt a {dealt_card}")
-    
+  
+# Check if player has bust or not
 def bust(player):
     if players[player]['card_total'] > 21:
         players[player]['hit_status'] = False
@@ -32,19 +30,10 @@ def bust(player):
     else:
         return False
         
-    
+# calculate players hand total for int 
 def card_total(player,add_card):
     global players
-    #card_totals[player] = 
     players[player]['card_total'] += add_card
-    #sum = 0
-    #for card in player_hand[player]:
-    #    card = royalty(card)
-    #    sum = sum + card
-    #print(card_totals)
-    #card_totals[player] = sum
-    #bust(player)
-    #return sum
     
 # take the count of cards in the deck and insert into a list that the "dealer" can deal from
 def deal_deck():
@@ -54,8 +43,8 @@ def deal_deck():
         for count in range(decks[card]):
             dealer_cards.append(card)
     random.shuffle(dealer_cards)
-    #print(dealer_cards)
 
+# Check for royal cards or Ace and appropriately convert to int value
 def royalty(player_name,card):
     royal_cards = ['Jack', 'Queen', 'King']
     if card in royal_cards:
@@ -66,10 +55,8 @@ def royalty(player_name,card):
     else:
         return card
 
+# Hit for the players
 def player_hit(player_name):
-    #while not game_end():
-    #while players[player_name]['hit_status'] == True:
-        #for player in players and not players['computer'] and players
     player_hand(player_name)
     if players[player_name]['hit_status'] == True:
         hit_choice = input('Would you like to hit? (y/n)\n')
@@ -84,16 +71,16 @@ def player_hit(player_name):
             return
     else:
         return
-
+# hit for the dealer
 def computer_hit():
     while players['computer']['card_total'] < 17:
         deal('computer')
         print(f"Dealer has: {players['computer']['cards_in_hand']}")
     return
 
+# Deal until all players no longer want to hit
 def continue_dealing():
     keep_dealing = True
-    #while players_done:
     for player in players:
         if players[player]['hit_status'] == True:
             return True
@@ -101,15 +88,19 @@ def continue_dealing():
             keep_dealing = False
     return keep_dealing
 
+# Easily print the players hand
 def player_hand(player_name):
     print(f"{player_name} you have: {players[player_name]['cards_in_hand']}\nCard total: {players[player_name]['card_total']}")
 
+# Extract the players from the dealer in the players dict
 def human_players():
     humans = []
     for player in players:
         if player != 'computer':
             humans.append(player)
     return humans
+
+# calculate per player if they have won or not
 def end_game():
     for player in human_players():
         player_total = int(players[player]['card_total'])
@@ -124,6 +115,7 @@ def end_game():
             print(f"{player} your {player_total} loses to the dealers {dealer_total}. ")
 
 def game_play():
+    # create players and decks to play from
     game_setup()
     # hit players
     keep_dealing = True
@@ -132,30 +124,16 @@ def game_play():
             player_hit(player)
             bust(player)  
         keep_dealing = continue_dealing()
-        #print(keep_dealing)
+    # hit dealer
     computer_hit()
+    # calculate winers
     end_game()
-    
-        
 
 decks = {2 : 4,3 : 4,4 : 4,5 : 4, 6 : 4,7 : 4,8 : 4,9 : 4,10 : 4, 'Jack' : 4, 'Queen' : 4, 'King' : 4, 'Ace' : 4}
-#player_hand = {'player1' : [], 'computer' : []}
 dealer_cards = []
 card_totals = {}
 players = {}
-players_at_table = human_players()
 
 
-
+# start the game
 game_play()
-
-
-#deal_deck()
-#deal('a')
-#deal('a')
-#print(players)
-#deal('player1')
-#deal('player1')
-#deal('player1')
-#print(card_totals)
-#print(royalty(card=2))
